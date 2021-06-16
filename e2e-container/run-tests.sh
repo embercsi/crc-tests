63855f937b9f0c61c3c26acfe5b1a1b8881a5af5
Gorka Eguileor <geguileo@redhat.com>
2021-06-16 14:54:53 +0200

    Support e2e parameters
    This patch adds 2 parameters that can be passed to the e2e tests:
    
    - number of times to repeat the tests
    - number of concurrent tests to run

--- e2e-container/run-tests.sh
+++ e2e-container/run-tests.sh
@@ -3,7 +3,10 @@ set -e
 set -x
 set -o pipefail
 
+count=${1:-1}
+parallel=${2:-0}
+
 # Filter out the tests we don't care about to make the test run faster
 ./extended-platform-tests run openshift/csi --dry-run | ./filter-tests.py > tests.txt
 # Now use the tests to create a suite
-./extended-platform-tests run openshift/csi --file tests.txt
+./extended-platform-tests run openshift/csi --count $count --max-parallel-tests $parallel --file tests.txt
